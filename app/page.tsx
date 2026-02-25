99d84372f4eaf90533d62d4db9793e21fa64f6df
rossdua <rcbrito2012@gmail.com>
2026-02-25 01:09:30 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,10 @@
 import Navbar from '@/components/Navbar';
 import HeroSection from '@/components/HeroSection';  
 import QuienesSomosSection from '@/components/QuienesSomosSection';         // ← Inglés
-import FeaturesSection from '@/components/FeaturesSection'; 
 import ServicesSection from '@/components/ServicesSection';
 import AliadosSection from '@/components/AliadosSection';  
 import AcademicoSection from '@/components/AcademicoSection';
 import NuestroEquipoSection from '@/components/NuestroEquipoSection'; // ← Inglés  
-import PricingSection from '@/components/PricingSection';
 import UbicacionSection from '@/components/UbicacionSection';     // ← Inglés
 import CTASection from '@/components/CTASection';             // ← Inglés
 import Footer from '@/components/Footer';                     // ← Inglés
@@ -23,8 +21,6 @@ export default function LandingPage() {
       <AliadosSection id="aliados" />
       <AcademicoSection id="academico" />
       <NuestroEquipoSection id="nuestro-equipo" />
-      {/* <FeaturesSection id="caracteristicas" /> */}
-      {/* <PricingSection id="precios" /> */}
       <UbicacionSection id="ubicacion" />
       <CTASection id="contacto" />
       <Footer />
